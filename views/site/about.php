6eeae782bc4472941359768b19150db8de63a253
jasonzh <939805498@qq.com>
2016-01-11 15:40:34 +0800

    1

---
+++ views/site/about.php
@@ -0,0 +1,22 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About Us';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+    </p>
+
+    <code><?= __FILE__ ?></code>
+    
+    <p>
+    <?php //echo Yii::app; ?>
+    </p>
+</div>
